dd6ee8ba26528c2b410286bf9ab318e00714d2a6
Aman Kalabay <230103375@sdu.edu.kz>
2024-06-24 15:12:49 +0500

    aman

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Player from "./component/Player";
+function App(){
+  return(
+    <>
+      <Player/>
+      <div id="challenges">
+      
+      </div>
+    </>
+  )
+}
+export default App;
---
+++ src/component/Player.jsx
@@ -0,0 +1,4 @@
+function Player(){
+    
+}
+export default Player
